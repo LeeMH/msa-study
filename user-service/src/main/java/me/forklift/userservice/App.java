a3d82f9eeadc59cf23ef441767004189e330ca22
mhlee <lee.munhaeng@gmail.com>
2021-06-30 15:55:17 +0900

    security 설정 추가

--- user-service/src/main/java/me/forklift/userservice/App.java
+++ user-service/src/main/java/me/forklift/userservice/App.java
@@ -3,10 +3,17 @@ package me.forklift.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class App {
+
+	@Bean
+	BCryptPasswordEncoder getEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
