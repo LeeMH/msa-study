1cdf9e215ea88bec1cdb1f8f1c38360791de4817
mhlee <lee.munhaeng@gmail.com>
2021-07-06 15:36:56 +0900

    api gateway 필터 추가

--- user-service/src/main/java/me/forklift/userservice/vo/RequestUser.java
+++ user-service/src/main/java/me/forklift/userservice/vo/RequestUser.java
@@ -1,9 +1,12 @@
 package me.forklift.userservice.vo;
 
+import lombok.Data;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Data
 public class RequestUser {
     @NotNull(message = "email cannot be null")
     @Size(min = 2, message = "email not be less than 2 characters")
